8a11fce59d1704bf0b51784ff3e669749e9be4bf
MatheusXz <49512638+MatheusXz@users.noreply.github.com>
2026-09-10 02:15:18 -0300

    feat: amplia area util da comanda

--- comandas-lanchonete-frontend/src/app/admin/comandas/[id]/page.jsx
+++ comandas-lanchonete-frontend/src/app/admin/comandas/[id]/page.jsx
@@ -1,13 +1,16 @@
 import Can from "@/components/ui/can/Can";
 import AccessDenied from "@/components/ui/accessDenied";
 import ComandaDetalhesClient from "./ComandaDetalhesClient";
+import styles from "./page.layout.module.css";
 
 export const metadata = { title: "Gerenciar Comanda | Admin" };
 
 export default function ComandaDetalhesPage() {
     return (
         <Can perform="comandas.listar" fallback={<AccessDenied />}>
-            <ComandaDetalhesClient />
+            <div className={styles.comandaLayout}>
+                <ComandaDetalhesClient />
+            </div>
         </Can>
     );
 }
